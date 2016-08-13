3899f505628cfda754c8599987618dda47194e30
Tom Manderson <me@trm.io>
2016-08-13 20:24:57 +1000

    All program structure completed
    Everything but the actual algorithms we want has been implemented.

---
+++ a1/util.py
@@ -0,0 +1,72 @@
+from typing import Dict, List, Tuple
+from enum import IntEnum
+
+
+def matrix_from_filecontents(data: str) -> List[List[float]]:
+
+    return [
+        [
+            float(elem.strip()) for elem in row.split(" ")
+        ]
+        for row in data.split("\n")[1:]
+    ]
+
+
+def graph_from_matrix(
+        data: List[List[float]],
+        directed: bool = False
+) -> Dict[int, Dict[int, float]]:
+    g = {}  # type: Dict[int, Dict[int, float]]
+    for i, row in enumerate(data):
+        for j, weight in enumerate(row):
+            if i not in g:
+                g[i] = {}
+            if directed and j not in g:
+                g[j] = {}
+            if weight <= 0:
+                continue
+            g[i][j] = weight
+            if directed:
+                g[j][i] = weight
+    return g
+
+
+class Algorithm(IntEnum):
+    uniform = 0
+    astar = 1
+
+    def __str__(self) -> str:
+        if self == Algorithm.uniform:
+            return "Uniform"
+        elif self == Algorithm.astar:
+            return "A*"
+        else:
+            raise ValueError("Invalid enum")
+
+
+class Query(object):
+    def __init__(self, alg: Algorithm, initial: int, goal: int) -> None:
+        self.alg = alg
+        self.initial = initial
+        self.goal = goal
+
+    def __str__(self) -> str:
+        return "{} {} {}".format(str(self.alg), self.initial, self.goal)
+
+
+def query_from_string(data: str) -> Query:
+    str_alg, initial, goal = data.split()
+    if str_alg == "A*":
+        alg = Algorithm.astar  # type: Algorithm
+    elif str_alg == "Uniform":
+        alg = Algorithm.uniform  # type: Algorithm
+
+    initial = int(initial)
+
+    goal = int(goal)
+
+    return Query(alg, initial, goal)
+
+
+def res_to_result(data: List[int]) -> str:
+    return "-".join(map(str, data))
