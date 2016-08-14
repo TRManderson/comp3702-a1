66e60b17ddc99e4c85720ac3a6912cdd58dc7a3a
Tom Manderson <me@trm.io>
2016-08-14 10:05:30 +1000

    Apparently I accidentally used tabs

--- a1/problem.py
+++ a1/problem.py
@@ -2,17 +2,17 @@ from typing import Dict, List
 import a1.util as u
 
 class Problem(object):
-	def __init___(self, graph: Dict[int, Dict[int, float]]) -> None:
-		self.graph = graph
+    def __init___(self, graph: Dict[int, Dict[int, float]]) -> None:
+        self.graph = graph
 
 
-	def query(self, query: u.Query) -> List[int]:
-		result = getattr(self, query.alg.name)(query.initial, query.goal) # type: List[int]
-		return result
+    def query(self, query: u.Query) -> List[int]:
+        result = getattr(self, query.alg.name)(query.initial, query.goal) # type: List[int]
+        return result
 
-	def astar(self, initial, goal):
-		pass
+    def astar(self, initial, goal):
+        visited = set()
 
 
-	def uniform(self, initial, goal):
-		pass
+    def uniform(self, initial, goal):
+        pass
